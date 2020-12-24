1379bc802c4ddc34e82a1c190560b3559086fb57
Jonathan Kenton <jonathankenton@gmail.com>
2020-12-24 15:03:39 +0200

    feat: create loading effect on landing page

--- apps/frontendsamurai/src/pages/index.tsx
+++ apps/frontendsamurai/src/pages/index.tsx
@@ -2,33 +2,59 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
-
-
-background-color: black;
-font-family: 'Ubuntu', sans-serif;
+  background-color: #000;
+  font-family: 'Ubuntu', 'Roboto', 'Helvetica', 'Arial', sans-serif;
   position: absolute;
   width: 100%;
   height: 100%;
-  /* display: flex; */
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
 `;
 
 const StyledHeader = styled.header`
-  /* display: flex;
-  align-items: center;
-  justify-content: center; */
 
-  &.invertColors {
-    color: white;
+  position: absolute;
+  text-align: center;
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+    10% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  &.permanentHeader {
+    color: #fff;
+    animation: 6s ease-in-out 0s 1 fadeIn;
+    opacity: 1;
+  }
+
+  &.initialHeader {
+    color: #fff;
+    animation: 3s ease-in-out 0s 1 fadeOut;
+    opacity: 0;
   }
 
 `;
 
 const Screen = styled.div`
-width: 100%;
-height: 100vh;
+  width: 100%;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -39,15 +65,19 @@ const Index = () => {
   return (
     <StyledApp>
       <Screen>
-    <StyledHeader className="invertColors" >
-      <h1>DON'T BE A NINJA</h1>
-    </StyledHeader>
-    </Screen>
-    <Screen>
-    <StyledHeader>
-      <h1>BE A SAMURAI</h1>
-    </StyledHeader>
-    </Screen>
+        <StyledHeader className="initialHeader" >
+          <h1>DON'T BE A NINJA</h1>
+        </StyledHeader>
+        <StyledHeader className="permanentHeader">
+          <h1>BE A FRONTEND SAMURAI</h1>
+        </StyledHeader>
+      </Screen>
+      <Screen>
+        <StyledHeader>
+          <h1>Frontend Samurai Academy</h1>
+          <h3>coming soon...</h3>
+        </StyledHeader>
+      </Screen>
     </StyledApp>
   );
 };
