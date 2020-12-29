be4b37faea4193f63d5247118e843efe318461be
Jonathan Kenton <jonathankenton@gmail.com>
2020-12-29 19:57:11 +0200

    fix: darken category colors

--- apps/frontendsamurai/src/data/colors.tsx
+++ apps/frontendsamurai/src/data/colors.tsx
@@ -1,13 +1,7 @@
 
 export const indigo: string[] = [
-  "#1a237e",
-  "#283593",
-  "#303f9f",
-  "#3949ab",
-  "#3f51b5",
-  "#5c6bc0",
-  "#7986cb",
-  "#9fa8da",
-  "#c5cae9",
-  "#e8eaf6",
+  "#010215", // yourself
+  "#01021c", // your team
+  "#010322", // leadership
+  "#010429", // balance
 ]
