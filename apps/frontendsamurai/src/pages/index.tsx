dac40db4fbdaad82da183f668dc538f512013f63
Jonathan Kenton <jonathankenton@gmail.com>
2020-12-29 20:22:01 +0200

    style: remove commented out line

--- apps/frontendsamurai/src/pages/index.tsx
+++ apps/frontendsamurai/src/pages/index.tsx
@@ -4,8 +4,6 @@ import { categories as categoriesList } from '../data/categories-list'
 import Category from '../components/category';
 import Screen from '../components/screen'
 
-// import 'fonts/fonts.css';
-
 const StyledApp = styled.div`
   background-color: #000;
   color: #fff;
