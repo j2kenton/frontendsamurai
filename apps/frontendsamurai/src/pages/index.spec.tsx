3a33bb47f0f689b825a09c2c61baaddff676fa14
Jonathan Kenton <jonathankenton@gmail.com>
2021-01-19 16:18:48 +0200

    test: update index spec file

--- apps/frontendsamurai/src/pages/index.spec.tsx
+++ apps/frontendsamurai/src/pages/index.spec.tsx
@@ -8,6 +8,6 @@ describe('Index', () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const props: any = {};
     const { getByText } = render(<Index {...props} />);
-    expect(getByText('Welcome to frontendsamurai!')).toBeTruthy();
+    expect(getByText(`DON'T BE A NINJA`)).toBeTruthy();
   });
 });
